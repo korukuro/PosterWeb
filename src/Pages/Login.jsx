1ae0774233fb7a5dd386a832522a0079761a4d00
Siddharth <siddharthtomar003@gmail.com>
2024-12-29 20:17:15 +0530

    login page and title of the tab login form {styling} inserted image in additional file and also font style link in index.html

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,15 +1,16 @@
 import Template from "../components/core/auth/Template";
-
+import logo from "../additionalFile/logo.png";
 function Login() {
   return (
     <div className="h-screen flex overflow-hidden">
       <div className="w-[55%] border-black border-2 flex-shrink-0 background-black">
         <p>HEELo</p>
       </div>
-      <div className="w-[45%] mx-auto border-black border-2 flex items-center justify-center flex-shrink-0">
-        <img src="" alt="" />
+      <div className="w-[45%] mx-auto border-black border-2 relative ">
+          {/* <img src={logo} alt="" className="ml-[35%]"/> */}
+        
         <Template
-          title="Welcome Back"
+          title="Welcome Back!!"
           formType="login"
         />
       </div>
--- src/components/core/auth/LoginForm.jsx
+++ src/components/core/auth/LoginForm.jsx
@@ -1,42 +1,46 @@
-import { useState } from "react"
-import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai"
-import { useDispatch } from "react-redux"
-import { Link, useNavigate } from "react-router-dom"
+import { useState } from "react";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import "../../../font/font.css";
 
-import { login } from "../../../services/operations/authAPI"
+import { login } from "../../../services/operations/authAPI";
+import googleLogo from "../../../additionalFile/google.png";
+import facebookLogo from "../../../additionalFile/facebook.png";
+
 
 function LoginForm() {
-  const navigate = useNavigate() //what is useNavigate? It is a hook that returns a navigate function that can be used to programmatically navigate to a different location.eg: navigate('/login')
-  const dispatch = useDispatch() //what is dispatch? It is a function that is used to dispatch actions to the Redux store.eg: dispatch(login(email, password, navigate))
+  const navigate = useNavigate(); //what is useNavigate? It is a hook that returns a navigate function that can be used to programmatically navigate to a different location.eg: navigate('/login')
+  const dispatch = useDispatch(); //what is dispatch? It is a function that is used to dispatch actions to the Redux store.eg: dispatch(login(email, password, navigate))
   const [formData, setFormData] = useState({
     email: "",
     password: "",
-  })
+  });
 
-  const [showPassword, setShowPassword] = useState(false)
+  const [showPassword, setShowPassword] = useState(false);
 
-  const { email, password } = formData
+  const { email, password } = formData;
 
   const handleOnChange = (e) => {
     setFormData((prevData) => ({
       ...prevData,
       [e.target.name]: e.target.value,
-    }))
-  }
+    }));
+  };
 
   const handleOnSubmit = (e) => {
-    e.preventDefault()
-    dispatch(login(email, password, navigate))
-  }
+    e.preventDefault();
+    dispatch(login(email, password, navigate));
+  };
 
   return (
     <form
       onSubmit={handleOnSubmit}
-      className="mt-6 flex w-full flex-col gap-y-4"
+      className="mt-10 flex w-full flex-col gap-y-4 rounded-[8px] ml-9 "
     >
-      <label className="w-full top-[47.194rem] border-2 border-black">
-        <p className="mb-1 text-[0.875rem] leading-[1.375rem] text-richblack-5 ">
-          E-mail 
+      <label className="w-full top-[47.194rem] ">
+        <p className="mb-1 text-[1.2rem] leading-[1.375rem] text-richblack-5 ">
+          E-mail
         </p>
         <input
           required
@@ -45,12 +49,12 @@ function LoginForm() {
           value={email}
           onChange={handleOnChange}
           placeholder="Enter email address"
-          className="form-style w-full"
+          className="form-style w-full bg-gray-300 rounded-lg text-gray-700 p-2 border-2 border-black"
         />
       </label>
-      <label className="relative">
-        <p className="mb-1 text-[0.875rem] leading-[1.375rem] text-richblack-5">  
-          PASSWORD 
+      <label className="relative mt-4">
+        <p className="mb-1 text-[1.2rem] leading-[1.375rem] text-richblack-5">
+          PASSWORD
         </p>
         <input
           required
@@ -59,32 +63,47 @@ function LoginForm() {
           value={password}
           onChange={handleOnChange}
           placeholder="Enter Password"
-          className="form-style w-full !pr-10"
+          className="form-style w-full bg-gray-300 rounded-lg text-gray-700 p-2 border-2 border-black"
         />
         <span
           onClick={() => setShowPassword((prev) => !prev)}
-          className="absolute right-3 top-[38px] z-[10] cursor-pointer"
+          className="absolute right-3 top-[35px] z-[10] cursor-pointer"
         >
           {showPassword ? (
-            <AiOutlineEyeInvisible fontSize={24} fill="#AFB2BF" />
+            <AiOutlineEyeInvisible fontSize={24} fill="#111111" />
           ) : (
-            <AiOutlineEye fontSize={24} fill="#AFB2BF" />
+            <AiOutlineEye fontSize={24} fill="#111111" />
           )}
         </span>
-        <Link to="/forgot-password">
-          <p className="mt-1 ml-auto max-w-max text-xs text-blue-100">
-            Forgot Password
-          </p>
-        </Link>
       </label>
-      <button
-        type="submit"
-        className="mt-6 rounded-[8px] bg-yellow-50 py-[8px] px-[12px] font-medium text-richblack-900"
-      >
-        Sign In
-      </button>
+      <div className="flex items-center justify-between w-full mt-6">
+        <Link to="/forgot-password">
+          <p className="text-base text-blue-700">forgot Password?</p>
+        </Link>
+
+        <button
+          type="submit"
+          className="rounded-l-full rounded-r-full bg-black text-white py-[0.6rem] px-[0.90rem] font-bold"
+        >
+          LOGIN
+        </button>
+      </div>
+      <div className="flex items-center my-4">
+        <div className="flex-grow border-t border-black"></div>
+        <span className="mx-1 text-gray-700 font-semibold text-xl">or</span>
+        <div className="flex-grow border-t border-black"></div>
+      </div>
+      <div className="flex items-center justify-evenly">
+        <span>Login With</span>
+        <div className="flex items-center">
+          <img src={googleLogo} alt="google logo" width={18}/><span className="text-blue-700 ml-1 text-lg">Google</span>
+        </div>
+        <div className="flex items-center">
+          <img src={facebookLogo} alt="facebook logo" width={18}/><span className="text-blue-700 ml-1 text-lg">facebook</span>
+        </div>
+      </div>
     </form>
-  )
+  );
 }
 
-export default LoginForm
+export default LoginForm;
