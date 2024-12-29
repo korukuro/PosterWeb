fee91ab2600dd1f9d5723d444b6a965deab9e642
Siddharth <siddharthtomar003@gmail.com>
2024-12-29 17:00:39 +0530

    Avenger Assemble

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -3,10 +3,10 @@ import Template from "../components/core/auth/Template";
 function Login() {
   return (
     <div className="h-screen flex overflow-hidden">
-      <div className="w-[60.713rem] border-black border-2 flex-shrink-0">
+      <div className="w-[55%] border-black border-2 flex-shrink-0 background-black">
         <p>HEELo</p>
       </div>
-      <div className="w-[44.950rem] mx-auto border-black border-2 flex items-center justify-center flex-shrink-0">
+      <div className="w-[45%] mx-auto border-black border-2 flex items-center justify-center flex-shrink-0">
         <img src="" alt="" />
         <Template
           title="Welcome Back"
