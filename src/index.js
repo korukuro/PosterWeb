387bfc0f3ef8bd32a035bc9e5c2e17692c207a27
ShivanshJadly <shivanshjadly0@gmail.com>
2024-12-30 01:15:25 +0530

    nothing major

--- src/index.js
+++ src/index.js
@@ -3,18 +3,22 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
-import { Store } from "./redux/Store";
+
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./reducer/index";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const store = configureStore({
+  reducer:rootReducer,
+});
 
 root.render(
-  <BrowserRouter>
-    <Provider store={Store}>
-      <App />
-      <Toaster/>
-    </Provider>
-    
-  </BrowserRouter>
+  <Provider store = {store}>
+    <BrowserRouter>
+        <App />
+        <Toaster/>
+      </BrowserRouter>
+  </Provider>
 );
