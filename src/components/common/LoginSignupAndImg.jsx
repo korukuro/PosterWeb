71bbf6fd8a74bf0b08a81df1d320e4f7677afdf3
ShivanshJadly <shivanshjadly0@gmail.com>
2024-12-30 13:41:13 +0530

    hover login and signup

--- src/components/common/LoginSignupAndImg.jsx
+++ src/components/common/LoginSignupAndImg.jsx
@@ -1,26 +1,44 @@
 import React from 'react'
 import leftImage from '../../additionalFile/collage2.jpg'
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Template from '../core/auth/Template';
 const LoginSignupAndImg = () => {
     const { token } = useSelector((state) => state.auth || {});
+    const location = useLocation();
 
+    const vistcheck = location.pathname === "/login" ? "Login" : "Signup";
     return (
       <div className="h-screen">
         {!token && (
           <>
             {/* Buttons Section */}
-            <div className="flex flex-col items-center justify-center mt-8 right-[34.5%] top-[23%] fixed">
+            <div className="flex flex-col gap-4 items-center justify-center mt-8 right-[34.5%] top-[23%] fixed">
               <Link to="/login">
-                <button className="bg-[#00000053] text-white pl-5 w-[7.5rem] h-[3.5rem] py-2 rounded-l-full text-md font-bold">
-                  Login
-                </button>
+              {
+                vistcheck === "Login" ? (
+                  <button className="bg-red-600 text-white pl-5 w-[7.5rem] h-[3.5rem] py-2 rounded-l-full text-md font-bold">
+                    Login
+                  </button>
+                ) : (
+                  <button className="bg-[#00000053] text-white pl-5 w-[7.5rem] h-[3.5rem] py-2 rounded-l-full text-md font-bold">
+                    Login
+                  </button>
+                )
+              }
               </Link>
               <Link to="/signup">
-                <button className="bg-[#00000053] text-white pl-5 py-2 w-[7.5rem] h-[3.5rem] rounded-l-full text-md font-bold">
-                  Signup
-                </button>
+              {
+                vistcheck === "Signup" ? (
+                  <button className="bg-red-600 text-white pl-5 py-2 w-[7.5rem] h-[3.5rem] rounded-l-full text-md font-bold">
+                    Signup
+                  </button>
+                ) : (
+                  <button className="bg-[#00000053] text-white pl-5 py-2 w-[7.5rem] h-[3.5rem] rounded-l-full text-md font-bold">
+                    Signup
+                  </button>
+                )
+              }
               </Link>
             </div>
   
