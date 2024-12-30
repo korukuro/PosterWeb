4aff23d64095b5a794b1b9323785768230961d2f
Siddharth <siddharthtomar003@gmail.com>
2024-12-30 19:56:44 +0530

    my profile

--- src/components/core/Dashboard/MyProfile.jsx
+++ src/components/core/Dashboard/MyProfile.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const MyProfile = () => {
   return (
-    <div>
-      my profile
-    </div>
-  )
-}
+    <div className="h-[37.188rem] w-full flex flex-col">
+      {/* Heading at the top */}
+      <h1 className="text-xl font-bold mt-4">MY PROFILE</h1>
 
-export default MyProfile
+      {/* Bordered section below */}
+      <div className="flex-grow flex justify-center items-center border-2 border-white w-full mt-4 rounded-xl">
+        //fetch name and email from the user object 
+      </div>
+    </div>
+  );
+};
+
+export default MyProfile;
