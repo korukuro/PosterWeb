b6973005d4e407c32a0174bef72d4f2711fe69a4
ShivanshJadly <shivanshjadly0@gmail.com>
2024-12-30 13:32:48 +0530

    commit

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,11 +1,17 @@
+import LoginSignupAndImg from "../components/common/LoginSignupAndImg"
 import Template from "../components/core/auth/Template"
 
 function Signup() {
   return (
-    <Template
-      title="Sign UP!!"
-      formType="signup"
-    />
+    <div className="h-screen grid grid-cols-3">
+      <div className="col-span-2">
+        <LoginSignupAndImg />
+      </div>
+      <Template
+        title="Sign UP!!"
+        formType="signup"
+      />
+    </div>
   )
 }
 
