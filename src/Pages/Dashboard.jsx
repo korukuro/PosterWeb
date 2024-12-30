3293c72b1c339f7bfcf564496d040af68b82d3aa
ShivanshJadly <shivanshjadly0@gmail.com>
2024-12-30 20:09:14 +0530

    color

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -54,7 +54,7 @@ export function SidebarDemo() {
       {/* Sidebar */}
       <Sidebar open={open} setOpen={setOpen}>
         <SidebarBody className="justify-between gap-10">
-          <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+          <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden ">
             <div className="mt-8 flex flex-col gap-6">
               {links.map((link, idx) => (
                 <SidebarLink key={idx} link={link} />
@@ -87,7 +87,7 @@ export const LogoIcon = () => {
 const Dashboard = () => {
   return (
     <div className="flex flex-1 min-h-screen overflow-auto">
-      <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
+      <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-200 flex flex-col gap-2 flex-1 w-full h-full">
         <div className="flex gap-2">
           <Outlet />
         </div>
