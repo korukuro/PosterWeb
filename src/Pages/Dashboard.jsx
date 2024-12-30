ba05c36a5968d356a91407702784072f763b1d92
Siddharth <siddharthtomar003@gmail.com>
2024-12-31 01:35:15 +0530

    small change

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -111,7 +111,7 @@ const Dashboard = () => {
   return (
     <div className="flex flex-1 min-h-screen overflow-auto">
       <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-200 flex flex-col gap-2 flex-1 w-full h-full">
-      <BackgroundBeams />
+      {/* <BackgroundBeams /> */}
         <div className="flex gap-2">
           <Outlet />
         </div>
