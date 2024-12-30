a07daf5da561ead1354a8f2138165d70735446c3
ShivanshJadly <shivanshjadly0@gmail.com>
2024-12-31 00:51:13 +0530

    done

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -107,8 +107,8 @@ export const LogoIcon = () => {
 
 const Dashboard = () => {
   return (
-    <div className="flex flex-1 min-h-screen overflow-auto">
-      <div className="p-4 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
+    <div className="flex flex-1 min-h-screen overflow-auto text-white">
+      <div className="p-4 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-black/95 flex flex-col gap-2 flex-1 w-full h-full">
         <Outlet />
       </div>
     </div>
