08ffa31d40cf4b86587e0a7ab07bc390718ab0ed
ShivanshJadly <shivanshjadly0@gmail.com>
2025-01-05 23:40:40 +0530

    mc

--- src/Pages/PosterDetails.jsx
+++ src/Pages/PosterDetails.jsx
@@ -1,48 +1,82 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { TiPlus } from "react-icons/ti";
 import { TiMinus } from "react-icons/ti";
+import { useState } from "react";
+import { getPosterDetails } from "../services/operations/posterDetailsAPI";
+import { useLocation } from "react-router-dom";
+import Spinner from "../components/Spinner";
 
 const PosterDetails = () => {
-  return (
-    <div className="border-2 border-black h-[42.38rem] flex overflow-hidden">
-      <div className="w-[45%] border-blue-800 border-2 flex justify-center items-center">
-        <div className="border-2 border-yellow-700 h-[35rem] w-[28rem] text-center">
-          hi
-        </div>
-      </div>
-      <div className="flex flex-col w-[55%] gap-5 p-16">
-        <div className="flex items-center justify-between">
-          <h1 className="font-bold text-2xl">Name Of The Poster</h1>
-          <div className="w-10 text-2xl">*****</div>
-        </div>
-        <span>Price:</span>
-        <div className="space-x-4">
-          <span>Size:</span>
-          <button className="w-20 h-12 text-base bg-black text-white rounded-full font-semibold text-[12px] p-1 px-3 uppercase">
-            A4
-          </button>
-          <button className="w-20 h-12 text-base bg-black text-white rounded-full font-semibold text-[12px] p-1 px-3 uppercase">
-            A3
-          </button>
-          <button className="w-20 h-12 text-base bg-black text-white rounded-full font-semibold text-[12px] p-1 px-3 uppercase">
-            12x18
-          </button>
-          <button className="w-20 h-12 text-base bg-black text-white rounded-full font-semibold text-[12px] p-1 px-3 uppercase">
-            13x19
-          </button>
-        </div>
-        <div className="w-[100%] flex flex-col justify-center space-y-4 ml-20 translate-x-[6rem]">
-          <div className="border-2 border-black w-28 h-14 rounded-l-full rounded-r-full flex justify-evenly items-center">
-            <TiMinus />
-            <div>1</div>
-            <TiPlus />
-          </div>
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const posterId = useLocation().pathname.split("/")[2];
 
-          <button className="w-28 h-14 bg-black text-white rounded-full font-semibold text-[12px] p-1 px-3 uppercase">
-            Add to Cart
-          </button>
-        </div>
-      </div>
+  async function fetchProductData(posterId) {
+    setLoading(true);
+    try {
+      const data = await getPosterDetails(posterId);
+      setPosts(data || {});
+    } catch (error) {
+      console.error("Failed to fetch poster data:", error);
+      setPosts({});
+    }
+    setLoading(false);
+  }
+  useEffect(() => {
+    fetchProductData(posterId);
+  }, []);
+
+
+  return (
+    <div className="flex justify-evenly w-11/12 m-6">
+      {
+        loading ? 
+        <div className="flex justify-center items-center h-full w-full">
+                <Spinner />
+         </div>
+          :
+          <div className="flex gap-10">
+            <div className="w-[45%]flex justify-center items-center">
+              <div className="h-[35rem] w-[28rem] text-center">
+                <img src={posts?.posterDetails?.image} alt="poster-image" />
+              </div>
+            </div>
+            <div className="flex flex-col w-[55%] gap-5 p-16">
+              <div className="flex items-center justify-between">
+                <h1 className="font-bold text-2xl">{posts?.posterDetails?.posterName}</h1>
+                <div className="w-10 text-2xl">*****</div>
+              </div>
+              <span>{`Price: ₹${posts?.posterDetails?.price}`}</span>
+              <div className="space-x-4">
+                <span>Size:</span>
+                <button className="w-20 h-12 text-base bg-black text-white rounded-full font-semibold text-[12px] p-1 px-3 uppercase">
+                  A4
+                </button>
+                <button className="w-20 h-12 text-base bg-black text-white rounded-full font-semibold text-[12px] p-1 px-3 uppercase">
+                  A3
+                </button>
+                <button className="w-20 h-12 text-base bg-black text-white rounded-full font-semibold text-[12px] p-1 px-3 uppercase">
+                  12x18
+                </button>
+                <button className="w-20 h-12 text-base bg-black text-white rounded-full font-semibold text-[12px] p-1 px-3 uppercase">
+                  13x19
+                </button>
+              </div>
+              <div className="w-[100%] flex flex-col justify-center space-y-4 ml-20 translate-x-[6rem]">
+                <div className="border-2 border-black w-28 h-14 rounded-l-full rounded-r-full flex justify-evenly items-center">
+                  <TiMinus />
+                  <div>1</div>
+                  <TiPlus />
+                </div>
+
+                <button className="w-28 h-14 bg-black text-white rounded-full font-semibold text-[12px] p-1 px-3 uppercase">
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+          </div>
+      }
+
     </div>
   );
 };
