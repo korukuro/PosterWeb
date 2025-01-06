d47fa0cd0094366c8e7d69777cd0d420f954d024
Siddharth <siddharthtomar003@gmail.com>
2025-01-06 11:22:13 +0530

    footer

--- src/Pages/CheckOut.jsx
+++ src/Pages/CheckOut.jsx
@@ -2,117 +2,116 @@ import React from "react";
 
 const CheckOut = () => {
   return (
-    <div className="h-[91.2vh] flex">
-      <div className="w-[50%] h-full border-2 border-black pl-56 pt-16">
-        <h1 className="text-2xl mb-2">Delivery</h1>
-        <form className="flex w-[30rem] flex-col gap-y-4">
+    <div className="h-[91.2vh] flex flex-col lg:flex-row">
+      {/* Left Section */}
+      <div className="lg:w-[50%] w-full h-auto border-t-2 border-r border-black p-10 lg:pl-56 lg:pt-16">
+        <h1 className="text-2xl mb-4">Delivery</h1>
+        <form className="flex flex-col gap-y-4">
           <label>
             <input
               type="text"
               placeholder="INDIA"
-              className="w-80 h-12 px-2 border-2 border-black rounded "
+              className="w-full lg:w-80 h-12 px-2 border border-black rounded"
             />
           </label>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
             <label>
               <input
                 required
                 type="text"
                 name="firstName"
-                placeholder="first name"
-                className="form-style w-ful border-2 border-black rounded-lg h-12 pl-2"
+                placeholder="First Name"
+                className="form-style w-full border border-black rounded-lg h-12 pl-2"
               />
             </label>
             <label>
               <input
                 required
                 type="text"
-                placeholder="last name"
-                className="form-style w-full border-2 border-black rounded-lg h-12 pl-2"
+                placeholder="Last Name"
+                className="form-style w-full border border-black rounded-lg h-12 pl-2"
               />
             </label>
           </div>
-
-          <label className="w-full">
+          <label>
             <input
               required
               type="text"
               placeholder="Address"
-              className="form-style w-full border-2 border-black rounded-lg h-12 pl-2"
+              className="form-style w-full border border-black rounded-lg h-12 pl-2"
             />
           </label>
-          <div className="flex flex-wrap gap-4">
-            <label className="flex-1">
+          <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+            <label>
               <input
                 required
                 type="text"
-                name="firstName"
+                name="city"
                 placeholder="City"
-                className="form-style w-full border-2 border-black rounded-lg h-12 pl-2"
+                className="form-style w-full border border-black rounded-lg h-12 pl-2"
               />
             </label>
-            <label className="flex-1">
+            <label>
               <input
                 required
                 type="text"
                 placeholder="State"
-                className="form-style w-full border-2 border-black rounded-lg h-12 pl-2"
+                className="form-style w-full border border-black rounded-lg h-12 pl-2"
               />
             </label>
-            <label className="flex-1">
+            <label>
               <input
                 required
                 type="text"
-                placeholder="PIN code"
-                className="form-style w-full border-2 border-black rounded-lg h-12 pl-2"
+                placeholder="PIN Code"
+                className="form-style w-full border border-black rounded-lg h-12 pl-2"
               />
             </label>
           </div>
-
-          <div>
-            <label className="relative">
-              <input
-                required
-                type="text"
-                placeholder="Phone"
-                className="form-style w-full border-2 border-black rounded-lg h-12 pl-2"
-              />
-            </label>
+          <label>
+            <input
+              required
+              type="text"
+              placeholder="Phone"
+              className="form-style w-full border border-black rounded-lg h-12 pl-2"
+            />
+          </label>
+          <div className="flex justify-between mt-4 text-sm">
+            <span className="text-gray-500">Expected Delivery:</span>
+            <span className="text-gray-500">3-4 days</span>
+          </div>
+          <div className="mt-5">
+            <button
+              type="submit"
+              className="rounded-lg bg-black py-3 px-6 font-medium text-white w-full sm:w-full"
+            >
+              PAY NOW
+            </button>
           </div>
         </form>
-        <div className="mt-6 w-[30rem]">
-          <p>Method of payment</p>
-          <div className="">
-
-          </div>
-          <button
-            type="submit"
-            className="mt-6 rounded-[8px] bg-black py-[8px] px-[12px] font-medium text-white flex items-center justify-center space-x-2 w-full sm:w-auto"
-          >
-            <span>PAY NOW</span>
-          </button>
-        </div>
       </div>
-      <div className="w-[50%] h-full pl-12 pt-16 border-t-2 border-black border-b-2">
-        <div className="w-[35.5rem] space-y-4">
-          <div className="flex w-[40rem] space-x-2">
-            <div className="w-20 border-2 border-black h-24">image</div>
-            <div className="border-2 border-blue-900 h-24 w-[30rem] pt-7 flex justify-between p-2">
-              details
-              <div>price</div>
+
+      {/* Right Section */}
+      <div className="lg:w-[50%] w-full h-auto border-t-2 lg:border-t-2 border-black lg:pl-12 lg:pt-16">
+        <div className="w-full lg:w-[35.5rem] space-y-4">
+          <div className="flex space-x-4">
+            <div className="w-20 h-24 flex-shrink-0 flex justify-center items-center">Image</div>
+            <div className="h-24 flex-grow flex justify-between items-center p-4">
+              <span>Details</span>
+              <span>Price</span>
             </div>
           </div>
           <div className="flex justify-between text-lg">
-            Total Items
-            <div>number</div>
+            <span>Total Items:</span>
+            <span>Number</span>
           </div>
           <div className="flex justify-between text-sm">
-            Shipment
-            <div>price</div>
+            <span>Shipment:</span>
+            <span>Price</span>
           </div>
-          <div className="flex justify-between text-lg">
-            Total
-            <div>price</div>
+          <div className="flex justify-between text-lg font-bold">
+            <span>Total:</span>
+            <span>Price</span>
           </div>
         </div>
       </div>
