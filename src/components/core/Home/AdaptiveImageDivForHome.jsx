666679a0dab5d2c330943deb9b7aefeaa5a7ce07
ShivanshJadly <shivanshjadly0@gmail.com>
2025-01-23 01:43:43 +0530

    home adaptive div

---
+++ src/components/core/Home/AdaptiveImageDivForHome.jsx
@@ -0,0 +1,65 @@
+"use client";
+
+import React, { useEffect, useState, useRef } from "react";
+export function AdaptiveImageDivForHome({ images }) {
+  const [backgroundColor, setBackgroundColor] = useState("rgb(255, 255, 255)");
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const img = new window.Image();
+    img.crossOrigin = "Anonymous";
+    img.src = images;
+    img.onload = () => {
+      const canvas = canvasRef.current;
+      const ctx = canvas?.getContext("2d");
+      if (ctx && canvas) {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0, img.width, img.height);
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const dominantColor = getDominantColor(imageData.data);
+        setBackgroundColor(`rgb(${dominantColor.join(",")})`);
+      }
+    };
+  }, [images]);
+
+  const getDominantColor = (imageData) => {
+    const rgb = [0, 0, 0];
+    const count = imageData.length / 4;
+    for (let i = 0; i < imageData.length; i += 4) {
+      rgb[0] += imageData[i];
+      rgb[1] += imageData[i + 1];
+      rgb[2] += imageData[i + 2];
+    }
+    return rgb.map((v) => Math.round(v / count));
+  };
+
+  const getContrastYIQ = (color) => {
+    const rgb = color.match(/\d+/g);
+    if (!rgb || rgb.length !== 3) return "black";
+    const yiq =
+      (parseInt(rgb[0]) * 299 + parseInt(rgb[1]) * 587 + parseInt(rgb[2]) * 114) /
+      1000;
+    return yiq >= 128 ? "black" : "white";
+  };
+
+  const textColor = getContrastYIQ(backgroundColor);
+
+  return (
+    <div className="h-[25rem] w-full flex justify-center items-center">
+      <div
+        className=" p-4 rounded-lg shadow-lg transition-colors duration-300 h-full w-full"
+        style={{ backgroundColor, color: textColor }}
+      >
+        <div className="flex justify-center items-center h-full w-full">
+          <img
+            src={images}
+            alt={`poster-image}`}
+            className={`w-full h-full border-black border-4 object-cover`}
+          />
+        </div>
+      </div>
+      <canvas ref={canvasRef} style={{ display: "none" }} />
+    </div>
+  );
+}
