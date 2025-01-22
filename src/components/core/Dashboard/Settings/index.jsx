7ec856c0ea01431f254e77f1f860d43346c250f2
Siddharth <siddharthtomar003@gmail.com>
2025-01-22 22:48:11 +0530

    minor

--- src/components/core/Dashboard/Settings/index.jsx
+++ src/components/core/Dashboard/Settings/index.jsx
@@ -1,16 +1,14 @@
-import ChangeProfilePicture from "./ChangeProfilePicture"
-import DeleteAccount from "./DeleteAccount"
-import EditProfile from "./EditProfile"
-import UpdatePassword from "./UpdatePassword"
+import ChangeProfilePicture from "./ChangeProfilePicture";
+import DeleteAccount from "./DeleteAccount";
+import EditProfile from "./EditProfile";
+import UpdatePassword from "./UpdatePassword";
 
-import { BackgroundBeams } from "../../../../components/ui/background-beam";  
-
+import { BackgroundBeams } from "../../../../components/ui/background-beam";
 
 export default function Settings() {
   return (
     <div className="flex flex-col gap-8">
-      <h1 className="mb-14 text-3xl font-medium text-richblack-5">
-
+      <h1 className="mt-16 ml-12 text-3xl font-medium underline text-richblack-5">
         Edit Profile
       </h1>
       {/* Change Profile Picture */}
@@ -22,5 +20,5 @@ export default function Settings() {
       {/* Delete Account */}
       <DeleteAccount />
     </div>
-  )
+  );
 }
