6527a9eb27d1f12a4abaeb4eb791c25a6ad1d9d2
Siddharth <siddharthtomar003@gmail.com>
2025-01-23 14:31:15 +0530

    category

--- src/Pages/Categories.jsx
+++ src/Pages/Categories.jsx
@@ -1,42 +1,63 @@
-import React, { useEffect, useState } from 'react';
-import { getAllCategories } from '../services/operations/posterDetailsAPI';
+import React, { useEffect, useState } from "react";
+import { getAllCategories } from "../services/operations/posterDetailsAPI";
 import { FocusCards } from "../components/ui/Focus-card";
-import { set } from 'mongoose';
-import Spinner from '../components/Spinner';
+import { set } from "mongoose";
+import Spinner from "../components/Spinner";
+
+import { motion } from "framer-motion";
+import { AuroraBackground } from "../components/ui/background";
 
 const Categories = () => {
-    const [categories, setCategories] = useState([]);
-    const [cards, setCards] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [categories, setCategories] = useState([]);
+  const [cards, setCards] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const fetchCategories = async () => {
-            setLoading(true);
-            try {
-                const res = await getAllCategories();
-                setCategories(res.data);
+  useEffect(() => {
+    const fetchCategories = async () => {
+      setLoading(true);
+      try {
+        const res = await getAllCategories();
+        setCategories(res.data);
 
-                const generatedCards = res.data.map((category) => ({
-                    title: category.name,
-                    categoryId: category._id,
-                }));
-                setCards(generatedCards);
-            } catch (error) {
-                console.error("Error fetching categories:", error);
-            }
-            setLoading(false);
-        };
-        fetchCategories();
-    }, []);
-    return (
-        <div className='pt-24 p-6 bg-[#000000a2]'>
-            {
-            loading ? <div><Spinner/></div> : 
-             <FocusCards cards={cards} />
-            
-            }
+        const generatedCards = res.data.map((category) => ({
+          title: category.name,
+          categoryId: category._id,
+        }));
+        setCards(generatedCards);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+      setLoading(false);
+    };
+    fetchCategories();
+  }, []);
+  return (
+    <AuroraBackground>
+      <motion.div
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="relative -z-20 flex flex-col gap-4 items-center justify-center px-4"
+      ></motion.div>
+      <div className="pt-24 p-6">
+      <div className="text-3xl md:text-8xl font-bold dark:text-white text-center pb-10 tracking-widest">
+          Collections
         </div>
-    );
+
+        {loading ? (
+          <div>
+            <Spinner />
+          </div>
+        ) : (
+          <FocusCards cards={cards} />
+        )}
+      </div>
+    </AuroraBackground>
+  );
 };
 
 export default Categories;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,18 @@
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       gridAutoRows: {
-        custom: "25rem", // Custom row height
+        custom: "25rem", 
       },
       animation: {
         shine: "shine 1s",
         "infinite-scroll": "infinite-scroll 20s linear infinite",
+        aurora: "aurora 60s linear infinite",
       },
       keyframes: {
         shine: {
@@ -17,8 +22,26 @@ module.exports = {
           "0%": { transform: "translateX(0)" },
           "100%": { transform: "translateX(calc(-50% - 20px))" },
         },
+        aurora: {
+          from: {
+            backgroundPosition: "50% 50%, 50% 50%",
+          },
+          to: {
+            backgroundPosition: "350% 50%, 350% 50%",
+          },
+        },
       },
     },
   },
-  plugins: [require("tailwind-scrollbar-hide")],
+  plugins: [require("tailwind-scrollbar-hide"),addVariablesForColors],
 };
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
