53044f30abd3f93406c42b14516b54cfe93edfcb
ShivanshJadly <shivanshjadly0@gmail.com>
2025-01-27 16:44:06 +0530

    mc

--- src/components/common/skeleton/PosterDetailsSkeleton.jsx
+++ src/components/common/skeleton/PosterDetailsSkeleton.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const PosterDetailsSkeleton = () => {
   return (
     <div className="flex justify-evenly w-11/12 mx-auto overflow-x-hidden pt-16 overflow-y-hidden">
-      <div className="grid grid-cols-2 mt-8 mb-8 mx-4 gap-4">
+      <div className="grid lg:grid-cols-2 grid-cols-1 mt-8 mb-8 mx-4 gap-4">
         {/* Right Section */}
         <div className="h-[500px] w-full bg-gray-300 animate-pulse rounded-lg"></div>
 
         {/* Left Section */}
-        <div className="flex flex-col gap-5 p-16">
+        <div className="flex flex-col gap-5 lg:p-16 p-8">
           {/* Title and Rating */}
           <div className="h-10 w-3/4 bg-gray-300 animate-pulse rounded-md"></div>
           <div className="h-6 w-1/4 bg-gray-300 animate-pulse rounded-md"></div>
@@ -40,10 +40,10 @@ const PosterDetailsSkeleton = () => {
           {/* Quantity Selector */}
           <div className="flex gap-4 items-center">
             <div className="h-6 w-16 bg-gray-300 animate-pulse rounded-md"></div>
-            <div className="border border-gray-300 h-14 w-[30%] rounded-full flex justify-evenly items-center animate-pulse">
-              <div className="h-8 w-8 bg-gray-300 rounded-full"></div>
-              <div className="h-8 w-8 bg-gray-300 rounded-full"></div>
-              <div className="h-8 w-8 bg-gray-300 rounded-full"></div>
+            <div className="border-gray-300 h-14 w-[40%] gap-x-1 rounded-full flex justify-evenly items-center animate-pulse">
+              <div className="h-8 w-full bg-gray-300 rounded-full"></div>
+              <div className="h-8 w-full bg-gray-300 rounded-full"></div>
+              <div className="h-8 w-full bg-gray-300 rounded-full"></div>
             </div>
           </div>
 
