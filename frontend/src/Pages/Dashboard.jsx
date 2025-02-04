ec9c8800ceff94588e6cc2b9d609c39fe45a47da
Siddharth <siddharthtomar003@gmail.com>
2025-02-04 15:08:57 +0530

    minor

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -27,13 +27,8 @@ const Dashboard = () => {
     });
   };
   return (
-    <div>
-      <button 
-        onClick={handleLogout} 
-        className="absolute rounded-full bg-red-500 p-2 right-5 top-24 text-3xl text-white hover:scale-[1.1] z-50">
-        <MdOutlineLogout />
-      </button>
-    <div className="sm:h-auto mt-16 lg:mt-10 flex flex-col items-center lg:gap-2 lg:flex-row pb-1 sm:pb-2 relative lg:min-h-screen pt-24 lg:pt-28 grid-col">
+      
+    <div className="sm:h-auto mt-16 lg:mt-1 flex flex-col items-center lg:gap-2 lg:flex-row pb-1 sm:pb-2 relative lg:min-h-screen pt-24 lg:pt-28 grid-col">
       <section className="lg:w-[50%] gap-3 flex flex-col lg:items-end w-[95%]">
         <div className="border border-dashed border-black flex justify-center items-center lg:h-[70%] lg:w-[90%] rounded-lg lg:shadow-md">
           <EditProfile />
@@ -49,10 +44,14 @@ const Dashboard = () => {
       <div className="h-[38rem] lg:pt-1 lg:pb-1 sm:w-screen sm:overflow-x-hidden lg:w-auto overflow-y-auto scrollbar-hide lg:border-t-2 lg:border-b-2 border-black">
         <OrderHistory />
       </div>
+      <button 
+        onClick={handleLogout} 
+        className="absolute rounded-full bg-red-500 p-2 right-5 top-24 text-3xl text-white hover:scale-[1.1] z-50">
+        <MdOutlineLogout />
+      </button>
       {confirmationModal && (
         <ConfirmationModal modalData={confirmationModal} />
       )}
-    </div>
     </div>
   );
 
