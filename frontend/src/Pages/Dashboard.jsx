86c3a89527bfd9b68f4403ced28d2f1cc078ff27
Siddharth <siddharthtomar003@gmail.com>
2025-02-04 15:16:49 +0530

    minor

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -27,33 +27,35 @@ const Dashboard = () => {
     });
   };
   return (
-      
-    <div className="sm:h-auto mt-16 lg:mt-1 flex flex-col items-center lg:gap-2 lg:flex-row pb-1 sm:pb-2 relative lg:min-h-screen pt-24 lg:pt-28 grid-col">
-      <section className="lg:w-[50%] gap-3 flex flex-col lg:items-end w-[95%]">
-        <div className="border border-dashed border-black flex justify-center items-center lg:h-[70%] lg:w-[90%] rounded-lg lg:shadow-md">
-          <EditProfile />
-        </div>
-        <div className="border border-dashed border-black flex justify-center items-center lg:h-[70%] lg:w-[90%] rounded-lg lg:shadow-md">
-          <UpdatePassword />
-        </div>
-      </section>
-
-      <h1 className="border-b border-black mb-1 lg:hidden pl-3">
-        Order History
-      </h1>
-      <div className="h-[38rem] lg:pt-1 lg:pb-1 sm:w-screen sm:overflow-x-hidden lg:w-auto overflow-y-auto scrollbar-hide lg:border-t-2 lg:border-b-2 border-black">
-        <OrderHistory />
-      </div>
-      <button 
-        onClick={handleLogout} 
-        className="absolute rounded-full bg-red-500 p-2 right-5 top-24 text-3xl text-white hover:scale-[1.1] z-50">
+    <div>
+      <button
+        onClick={handleLogout}
+        className="absolute rounded-full bg-red-500 p-2 right-5 top-24 text-3xl text-white hover:scale-[1.1] z-50"
+      >
         <MdOutlineLogout />
       </button>
-      {confirmationModal && (
-        <ConfirmationModal modalData={confirmationModal} />
-      )}
+      <div className="sm:h-auto mt-16 lg:mt-10 flex flex-col items-center lg:gap-2 lg:flex-row pb-1 sm:pb-2 relative lg:min-h-screen pt-24 lg:pt-28 grid-col">
+        <section className="lg:w-[50%] gap-3 flex flex-col lg:items-end w-[95%]">
+          <div className="border border-dashed border-black flex justify-center items-center lg:h-[70%] lg:w-[90%] rounded-lg lg:shadow-md">
+            <EditProfile />
+          </div>
+          <div className="border border-dashed border-black flex justify-center items-center lg:h-[70%] lg:w-[90%] rounded-lg lg:shadow-md">
+            <UpdatePassword />
+          </div>
+        </section>
+
+        <h1 className="border-b border-black mb-1 lg:hidden pl-3">
+          Order History
+        </h1>
+        <div className="h-[38rem] lg:pt-1 lg:pb-1 sm:w-screen sm:overflow-x-hidden lg:w-auto overflow-y-auto scrollbar-hide lg:border-t-2 lg:border-b-2 border-black">
+          <OrderHistory />
+        </div>
+
+        {confirmationModal && (
+          <ConfirmationModal modalData={confirmationModal} />
+        )}
+      </div>
     </div>
   );
-
 };
 export default Dashboard;
