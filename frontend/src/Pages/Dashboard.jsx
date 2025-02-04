5bd265971084f88186462499a5ebf84cb54c4948
ShivanshJadly <shivanshjadly0@gmail.com>
2025-02-04 15:03:24 +0530

    mc

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -5,12 +5,35 @@ import UpdatePassword from "../components/core/Dashboard/Settings/UpdatePassword
 import { MdOutlineLogout } from "react-icons/md";
 import ConfirmationModal from "../components/common/ConfirmationModal";
 import { useDispatch } from "react-redux";
-import { useState } from "react";
 import { logout } from "../services/operations/authAPI";
+import { useState } from "react";
 
 const Dashboard = () => {
+  const [confirmationModal, setConfirmationModal] = useState(null);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    setConfirmationModal({
+      text1: "Are you sure?",
+      text2: "You will be logged out of your account.",
+      btn1Text: "Logout",
+      btn2Text: "Cancel",
+      btn1Handler: () => {
+        dispatch(logout(navigate));
+        setConfirmationModal(null);
+      },
+      btn2Handler: () => setConfirmationModal(null),
+    });
+  };
   return (
-    <div className="sm:h-auto flex flex-col items-center lg:gap-2 lg:flex-row pb-1 sm:pb-2 relative lg:min-h-screen pt-24 lg:pt-28 grid-col">
+    <div>
+      <button 
+        onClick={handleLogout} 
+        className="absolute rounded-full bg-red-500 p-2 right-5 top-24 text-3xl text-white hover:scale-[1.1] z-50">
+        <MdOutlineLogout />
+      </button>
+    <div className="sm:h-auto mt-16 lg:10 flex flex-col items-center lg:gap-2 lg:flex-row pb-1 sm:pb-2 relative lg:min-h-screen pt-24 lg:pt-28 grid-col">
       <section className="lg:w-[50%] gap-3 flex flex-col lg:items-end w-[95%]">
         <div className="border border-dashed border-black flex justify-center items-center lg:h-[70%] lg:w-[90%] rounded-lg lg:shadow-md">
           <EditProfile />
@@ -30,7 +53,7 @@ const Dashboard = () => {
         <ConfirmationModal modalData={confirmationModal} />
       )}
     </div>
-
+    </div>
   );
 
 };
