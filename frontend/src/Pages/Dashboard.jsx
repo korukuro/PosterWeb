90e74af3389a43b12d9b118659918355b1496fc0
Siddharth <siddharthtomar003@gmail.com>
2025-02-04 13:25:36 +0530

    minor

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { useDispatch } from "react-redux";
-import { Link, Outlet, useNavigate } from "react-router-dom"
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import { logout } from "../services/operations/authAPI";
 import ConfirmationModal from "../components/common/ConfirmationModal";
 import { useState } from "react";
@@ -8,41 +8,43 @@ import EditProfile from "../components/core/Dashboard/Settings/EditProfile";
 import UpdatePassword from "../components/core/Dashboard/Settings/UpdatePassword";
 
 const Dashboard = () => {
-
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [confirmationModal, setConfirmationModal] = useState(null);
 
   const handleLogOut = () => {
-      setConfirmationModal({
-        text1: "Are you sure?",
-        text2: "You will be logged out of your account.",
-        btn1Text: "Logout",
-        btn2Text: "Cancel",
-        btn1Handler: () => dispatch(logout(navigate)),
-        btn2Handler: () => setConfirmationModal(null),
-      })
-  }
+    setConfirmationModal({
+      text1: "Are you sure?",
+      text2: "You will be logged out of your account.",
+      btn1Text: "Logout",
+      btn2Text: "Cancel",
+      btn1Handler: () => dispatch(logout(navigate)),
+      btn2Handler: () => setConfirmationModal(null),
+    });
+  };
   return (
-    <div className="sm:h-auto flex flex-col items-center lg:gap-2 lg:flex-row pb-1 sm:pb-2 relative lg:min-h-screen pt-16 lg:pt-28">
-     
-      
-    
-      <section className="lg:w-[45%] gap-3 flex flex-col lg:items-end w-[95%]">
-        <div className="border border-dashed border-black flex justify-center items-center lg:h-[70%] lg:w-[90%] rounded-lg lg:shadow-md"><EditProfile/></div>
-        <div className="border border-dashed border-black flex justify-center items-center lg:h-[70%] lg:w-[90%] rounded-lg lg:shadow-md"><UpdatePassword/></div>
+    <div className="sm:h-auto flex flex-col items-center lg:gap-2 lg:flex-row pb-1 sm:pb-2 relative lg:min-h-screen pt-16 lg:pt-28 grid-col">
+      <section className="lg:w-[50%] gap-3 flex flex-col lg:items-end w-[95%]">
+        <div className="border border-dashed border-black flex justify-center items-center lg:h-[70%] lg:w-[90%] rounded-lg lg:shadow-md">
+          <EditProfile />
+        </div>
+        <div className="border border-dashed border-black flex justify-center items-center lg:h-[70%] lg:w-[90%] rounded-lg lg:shadow-md">
+          <UpdatePassword />
+        </div>
       </section>
 
-      <section className="lg:w-[50%] lg:pt-5 lg:pb-10 border-2 border-black">
-        <button onClick={handleLogOut} className="absolute">Log Out</button>
-        <h1 className="border-b border-black mb-1 lg:hidden pl-3">Order History</h1>
-        <div className=" h-[29.5rem] sm:w-screen sm:overflow-x-hidden lg:w-auto border-2 rounded-lg overflow-auto scrollbar-hide"><OrderHistory/></div>
-      </section>
-
-      {confirmationModal && (
-        <ConfirmationModal modalData={confirmationModal}/>
-      )}
+      {/* <button onClick={handleLogOut} className="absolute">
+        Log Out
+      </button> */}
+      <h1 className="border-b border-black mb-1 lg:hidden pl-3">
+        Order History
+      </h1>
+      <div className=" h-[29.5rem] sm:w-screen sm:overflow-x-hidden lg:w-auto border-2 lg:h-[] rounded-lg overflow-auto scrollbar-hide">
+        <OrderHistory />
+      </div>
+        
+      {confirmationModal && <ConfirmationModal modalData={confirmationModal} />}
     </div>
-  )
-}
-export default Dashboard
+  );
+};
+export default Dashboard;
